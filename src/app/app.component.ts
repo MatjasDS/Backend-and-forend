51ed0cae72bade6a51f5fb803a0bd80fb33489f0
Matthias Desimpele <matthias.desimpele.y9169@student.hogent.be>
2020-05-04 20:03:09 +0200

    Opdracht 1 voltooid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { APIService } from './api.service';
+import { typeWithParameters } from '@angular/compiler/src/render3/util';
 
 interface Users{
   name:string;
@@ -22,13 +23,15 @@ export class AppComponent {
 
   addUser:boolean = false;
   showUsers:boolean = false;
+  showNote:boolean = false;
   addNote:boolean = false;
-  showNotes:boolean = false;
   userList: Array<Users>;
   notesList: Array<Notes>;
   service: APIService;
   ingegevenNaamToevoegen: string;
-  displayedColumnsUsers: string[] = ['name','remove']; 
+  ingegevenNoteToevoegen: string;
+  displayedColumnsUsers: string[] = ['name','note','shownote','remove']; 
+  displayedColumnsNotes: string[] = ['content'];
 
 
   constructor(apiService: APIService){
@@ -45,21 +48,53 @@ export class AppComponent {
   });
 
   AddUserView = () => {
-      this.addUser=true;
+      this.addUser = true;
+      this.addNote = false;
+      this.showNote = false;
+  }
+
+  AddNoteView = (addNoteforUser:string) => {
+    this.ingegevenNaamToevoegen = addNoteforUser;
+    this.addNote = true;
+    this.addUser = false;
+    this.showNote = false;
   }
 
   AddUserComponent = () => {
     this.service.AddUser(this.ingegevenNaamToevoegen).subscribe((response) => {
       console.log(response);
       this.UserlistRefresh();
+      this.addUser = false;
     });
     
   }
 
+  AddNoteComponent = () => {
+    this.service.AddNote(this.ingegevenNoteToevoegen, this.ingegevenNaamToevoegen).subscribe((response) => {
+      console.log(response);
+      this.UserlistRefresh();
+      this.addNote = false;
+    })
+  }
+
+  ShowNoteComponent = (addNoteforUser:string) => {
+      this.ingegevenNaamToevoegen = addNoteforUser;
+    this.service.GetNotes(this.ingegevenNaamToevoegen).subscribe((data: Array<Notes>) => {
+      console.log(data);
+      this.notesList = data;
+      this.showNote = true;
+      this.addNote = false;
+      this.addUser = false;
+    });
+  }
+
   DeleteUserAndNotesComponent = (removeName: string) => {
       this.service.DeleteUserAndNotes(removeName).subscribe((response) => {
-        this.ingegevenNaamToevoegen = "",
-          this.UserlistRefresh();
+        this.ingegevenNaamToevoegen = "";
+        this.showNote = false;
+        this.addNote = false;
+        this.addUser = false;
+        this.UserlistRefresh();
       });
   }
 
